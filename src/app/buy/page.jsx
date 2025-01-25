1d3c67ba27d2714ffab1fe02a9f1fd078af7feaf
Mohamad Reza <mohamadireza107@gmail.com>
2025-01-25 16:46:33 +0330

    init

---
+++ src/app/buy/page.jsx
@@ -0,0 +1,157 @@
+"use client";
+
+import Button from "@/components/UI/Button";
+import Image from "next/image";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
+import border from "@/image/border.svg";
+import { useReceipt } from "@/store/productDetail";
+import { useEffect, useRef, useState } from "react";
+import html2canvas from "html2canvas-pro";
+
+export default function Page() {
+  const searchParams = useSearchParams();
+
+  const captureRef = useRef();
+  const router = useRouter();
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    if (searchParams.get("0[status]") === "1") {
+      setData([
+        {
+          name: "شماره فاکتور",
+          text: searchParams.get("0[OrderID]"),
+        },
+        {
+          name: "شماره پیگیری",
+          text: searchParams.get("0[TRACENO]"),
+        },
+        {
+          name: "نوع پرداخت",
+          text: "پرداخت آنلاین",
+        },
+        {
+          name: "تاریخ",
+          text: new Date().toLocaleDateString("fa-IR"),
+          second: new Date().toLocaleTimeString("fa-IR").slice(0, 5),
+        },
+        {
+          name: "مبلغ",
+          text: JSON.parse(searchParams.get("0[params]")).Amount,
+          second: "تومان",
+        },
+      ]);
+    }
+  }, [searchParams]);
+
+  const lists = [
+    {
+      name: "شماره فاکتور",
+      text: "۱۴۰۳۰۷۱۴۰۰۴-۱",
+    },
+    {
+      name: "شماره پیگیری",
+      text: "۱۲۳۴۵۶۷۸۹۰",
+    },
+    {
+      name: "نوع پرداخت",
+      text: "پرداخت آنلاین",
+    },
+    {
+      name: "تاریخ",
+      text: "۱۴۰۳/۰۴/۱۵",
+      second: "۱۲:۴۷",
+    },
+    {
+      name: "مبلغ",
+      text: "۲۲۲.۷۵۰.۰۰۰",
+      second: "تومان",
+    },
+  ];
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center gap-7 px-6">
+      <div
+        ref={captureRef}
+        className="relative bg-[#F7F7F7] w-full rounded-tl-2xl rounded-tr-2xl border-solid border-t border-x border-[#C2C2C2]"
+      >
+        <span className="icon-Share absolute top-4 right-4 text-xl text-default-500 cursor-pointer"></span>
+        <Image
+          className="absolute top-[100%] inset-x-0 w-full"
+          src={border}
+          alt="style"
+        />
+        {searchParams.get("0[status]") === "0" ? (
+          <div className="mt-6 flex flex-col gap-6 justify-center items-center mb-10">
+            <div className="flex justify-center items-center rounded-full w-[85px] h-[85px] bg-denger-900">
+              <span className="icon-light-bold-Close-Circle text-[85px] text-danger-900"></span>
+            </div>
+            <p className="text-xl font-bold text-danger-900">
+              {searchParams.get("0[message]")}
+            </p>
+          </div>
+        ) : (
+          <>
+            <div className="mt-6 flex flex-col gap-6 justify-center items-center">
+              <div className="flex justify-center items-center rounded-full w-[85px] h-[85px] bg-success">
+                <span className="icon-light-linear-Tick-success text-6xl"></span>
+              </div>
+              <p className="text-xl font-bold text-success">
+                پرداخت شما با موفقیت انجام شد.
+              </p>
+            </div>
+            <ul className="mt-10 mb-6 flex flex-col">
+              {data.map((list, index) => (
+                <li
+                  key={index + 1}
+                  className="flex justify-between items-center px-6 h-11"
+                >
+                  <p className="text-sm font-normal text-default-500">
+                    {list.name}
+                  </p>
+                  <div className="flex items-center gap-1">
+                    <p className="font-semibold text-default-900">
+                      {list.text}
+                    </p>
+                    <p className="text-sm font-normal text-default-500">
+                      {list.second}
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+      <div className="grid grid-cols-2 gap-3">
+        <Button
+          text={"ذخیره"}
+          type={
+            "w-full text-tertiary border-solid border-[2px] border-tertiary"
+          }
+          onClick={() => {
+            // addReceipt(lists);
+            const element = captureRef.current;
+            html2canvas(element, {
+              useCORS: true,
+              allowTaint: false,
+            }).then((canvas) => {
+              const link = document.createElement("a");
+              link.href = canvas.toDataURL("image/png");
+              link.download = "screenshot.png";
+              link.click();
+            });
+            // removeAllReceipt()
+          }}
+        />
+        <Button
+          text={"بازگشت به آگهی‌ها"}
+          type={
+            "w-full text-tertiary border-solid border-[2px] border-tertiary"
+          }
+          onClick={() => router.push("/")}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,224 @@
+"use client";
+
+import SaleCard from "@/components/Homepage/SaleCard";
+import BuyModal from "@/components/Modal/BuyModal";
+import { dayNames, monthNames } from "@/components/static";
+import Button from "@/components/UI/Button";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Loading from "./loading";
+import FilterLayout from "@/components/filters/FilterLayout";
+import moment from "jalali-moment";
+import PriceSuggestionModal from "@/components/Modal/PriceSuggestionModal";
+import ShareLoadLinkModal from "@/components/Modal/ShareLoadLinkModal";
+
+export default function Home() {
+  let j = moment();
+  const [selectedCard, setSelectedCard] = useState("");
+  const [provinces, setProvinces] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [filterValues, setFilterValues] = useState({
+    status: "",
+    origins: [],
+    print_types: [],
+    yolk_types: [],
+    qualities: [],
+  });
+
+  async function getLoads(lastID = "") {
+    setIsLoading(true);
+
+    await axios
+      .post(`${process.env.NEXT_PUBLIC_EGG_MARKET}/API/loads/loads`, {
+        status: filterValues.status,
+        print_types: filterValues.print_types,
+        yolk_types: filterValues.yolk_types,
+        qualities: filterValues.qualities,
+        cities: filterValues.origins.map((origin) => String(origin.id)),
+        origins: [],
+        pack_types: [],
+        types: [],
+        lastID: lastID, //برای صفحه بندی استفاده می شود. برای دریافت اطلاعات بیشتر یعنی صفحه بعد باید آخرین آیدی رو وارد کنید.
+      })
+      .then((response) => {
+        setIsLoading(false);
+        if (lastID) {
+          setData([...data, ...response.data.loads]);
+          setFilteredData([...data, ...response.data.loads]);
+        } else {
+          setData(response.data.loads);
+          setFilteredData(response.data.loads);
+        }
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+      });
+  }
+  useEffect(() => {
+    getLoads();
+  }, [filterValues]);
+
+  useEffect(() => {
+    async function getProvinces() {
+      await axios
+        .get(`${process.env.NEXT_PUBLIC_EGG_MARKET}/API/locations/provinces`)
+        .then((response) => {
+          setProvinces(response.data.provinces);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    getProvinces();
+  }, []);
+
+  // useEffect(() => {
+  //   let filtered = filteredData;
+  //   if (filterValues.available) {
+  //     filtered = filtered.filter((item) => item.status !== "sold");
+  //   } else if (filterValues.brand.length > 0) {
+  //     filtered = filtered.filter((item) => {
+  //       let brand = item.details.find(
+  //         (item) => item.title === "نام مجموعه"
+  //       )?.value;
+  //       if (BrandOptions.find((i) => i.title === brand)) return item;
+  //     });
+  //   } else if (
+  //     filterValues.weight.min !== 8 ||
+  //     filterValues.weight.max !== 14
+  //   ) {
+  //     filtered = filtered.filter((item) => {
+  //       let weight = item.details.find(
+  //         (item) => item.title === "وزن کارتن"
+  //       )?.value;
+  //       if (
+  //         Number(weight) > filterValues.weight.min &&
+  //         Number(weight) < filterValues.weight.max
+  //       )
+  //         return item;
+  //     });
+  //   } else {
+  //     filtered = data;
+  //   }
+  //   setFilteredData(filtered);
+  // }, [
+  //   data,
+  //   filterValues.available,
+  //   filterValues.brand,
+  //   filterValues.weight.min,
+  //   filterValues.weight.max,
+  // ]);
+
+  return (
+    <main>
+      <div className="sticky top-0 h-[55px] z-10 px-2 bg-surface-secondary filterShadow">
+        <FilterLayout
+          filterValues={filterValues}
+          setFilterValues={setFilterValues}
+        />
+      </div>
+      <div className="px-4 pb-32">
+        {isLoading && filteredData.length === 0 ? (
+          <Loading />
+        ) : filteredData.length === 0 ? (
+          <p className="text-center mt-4 text-default-500">باری وجود ندارد</p>
+        ) : (
+          <>
+            <div className="mb-4">
+              {filteredData.map((load, index, arr) => {
+                let isEqual = false;
+                if (index !== 0) {
+                  let previous = new Date(arr[index - 1].reg_just_date);
+                  previous = new Intl.DateTimeFormat("fa-IR").format(previous);
+                  let current = new Date(load.reg_just_date);
+                  current = new Intl.DateTimeFormat("fa-IR").format(current);
+                  isEqual = current === previous;
+                }
+                let date = new Date(load.reg_just_date);
+                date = new Intl.DateTimeFormat("fa-IR").format(date).split("/");
+                j = moment(load.reg_just_date);
+                return (
+                  <div key={index}>
+                    {isEqual ? (
+                      ""
+                    ) : (
+                      <>
+                        <div className="flex items-center justify-center gap-4 my-4">
+                          <hr className="w-full border-default-300" />
+                          <p className="text-sm font-semibold text-default-900 basis-1 text-nowrap">
+                            {`${dayNames[j.jDay()]} ${date[2]} ${
+                              monthNames[
+                                date[1].replace(/[۰-۹]/g, (d) =>
+                                  "۰۱۲۳۴۵۶۷۸۹".indexOf(d)
+                                ) - 1
+                              ]
+                            } ${date[0]}`}
+                          </p>
+                          <hr className="w-full border-default-300" />
+                        </div>
+                        <div className="flex items-center justify-between text-default-500 my-2">
+                          <p className="font-bold text-sm">
+                            تعداد بارهای اعلامی:
+                          </p>
+                          <p className="text-xs">{`${
+                            filteredData.filter(
+                              (item) =>
+                                item.reg_just_date === load.reg_just_date
+                            ).length
+                          } بارِ تخم‌مرغ`}</p>
+                        </div>
+                      </>
+                    )}
+                    <SaleCard
+                      key={load.loadID}
+                      load={load}
+                      province={provinces.find(
+                        (item) => item.id === load.origin_field1
+                      )}
+                      setSelectedCard={setSelectedCard}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+            <Button
+              type="button-primary-2"
+              text="مشاهده آگهی های بیشتر"
+              width="w-full"
+              onClick={() =>
+                getLoads(filteredData[filteredData.length - 1].loadID)
+              }
+              loading={isLoading}
+            />
+          </>
+        )}
+        <a
+          className="block"
+          referrerpolicy="origin"
+          target="_blank"
+          href="https://trustseal.enamad.ir/?id=529943&Code=E1c61yzJVW0wh3FeKLpQxVX7WxEEIkFB"
+        >
+          <img
+            referrerpolicy="origin"
+            src="https://trustseal.enamad.ir/logo.aspx?id=529943&Code=E1c61yzJVW0wh3FeKLpQxVX7WxEEIkFB"
+            alt=""
+            className="cursor-pointer size-24"
+            code="E1c61yzJVW0wh3FeKLpQxVX7WxEEIkFB"
+          />
+        </a>
+      </div>
+      <ShareLoadLinkModal
+        selectedCard={selectedCard}
+        setSelectedCard={setSelectedCard}
+      />
+      <PriceSuggestionModal
+        setSelectedCard={setSelectedCard}
+        selectedCard={selectedCard}
+      />
+      <BuyModal load={selectedCard} setSelectedCard={setSelectedCard} />
+    </main>
+  );
+}
